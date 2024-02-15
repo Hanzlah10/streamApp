60a7a1e6af2cb2364a3605c842b89087dda31b09
Hanzlah10 <hanzalasarguroh@gmail.com>
2024-02-15 21:35:48 +0530

    Added Refresh Token Endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
-import { Jwt } from "jsonwebtoken"
+import  Jwt  from "jsonwebtoken"
 
 
 const options = {
